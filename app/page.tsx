d16fceda69deb98e6e945dea6437bc4b80af97dc
Anubhav <96810206+anu24765@users.noreply.github.com>
2023-08-18 23:15:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
-
 "use client"
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+
+interface ChatEntry {
+    sender: string;
+    message: string;
+}
 
 export default function Home() {
     const [message, setMessage] = useState('');
-    const [chatHistory, setChatHistory] = useState([]);
-
+    const [chatHistory, setChatHistory] = useState<ChatEntry[]>([]);
 
     const sendMessage = async () => {
         if (message.trim() === '') return;
+
         setChatHistory([...chatHistory, { sender: 'user', message }]);
         setMessage('');
 
@@ -31,41 +36,43 @@ export default function Home() {
         }
     };
 
- 
-
     useEffect(() => {
         fetchChatHistory();
     }, []);
 
     return (
-      <div className=''>
-       <h1 className="text-4xl font-bold mb-8 text-center">Real-time Chat App with Python Backend</h1>
+        <div className=''>
+            <h1 className="text-4xl font-bold mb-8 text-center">
+                Real-time Chat App with Python Backend
+            </h1>
 
-      <div className="chat-container p-4 ">
-         
-          {chatHistory.map((entry, index) => (
-              <div key={index} className={`message mb-2 ${entry.sender}`}>
-                  {entry.message}
-              </div>
-          ))}
-      </div>
-      <div className="input-container p-4 bg-red shadow-md">
-          <input
-              type="text"
-              placeholder="Type your message..."
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="text-black p-2 border border-gray-300 rounded w-full"
-          />
-          <button
-              onClick={sendMessage}
-              className="bg-blue-500 text-white px-4 py-2 rounded ml-2"
-          >
-              Send
-          </button>
-      </div>
-  </div>
+            <div className="chat-container p-4">
+                {chatHistory.map((entry, index) => (
+                    <div
+                        key={index}
+                        className={`message mb-2 ${entry.sender}`}
+                    >
+                        {entry.message}
+                    </div>
+                ))}
+            </div>
+
+            <div className="input-container p-4 bg-red shadow-md">
+                <input
+                    type="text"
+                    placeholder="Type your message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="text-black p-2 border border-gray-300 rounded w-full"
+                />
+                <button onClick={sendMessage} className="bg-blue-500 text-white px-4 py-2 rounded ml-2">Send</button>
+            </div>
+
+            <footer className="text-center mt-8">
+                <p className="text-sm">Name : Anubhav</p>
+                <p className="text-sm">College : Noida Institute of Engineering and Technology</p>
+                <p className="text-sm">Branch : AI/ML</p>
+            </footer>
+        </div>
     );
 }
-
-
